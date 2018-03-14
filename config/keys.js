9f09d3ead18fcfa1ff9bf2098995b5f2a9788615
Emmanuel Singletary <me@manny.co>
2018-03-14 00:56:35 -0400

    Project restructure and stubs for our data sources

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,24 @@
 'use strict'
 
+/** Winston */
 module.exports.logging = {
   loglevel: process.env.LOGGING_LEVEL
+}
+
+/** MySQL */
+module.exports.database = {
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+}
+
+/** Google Sheets */
+module.exports.sheets = {
+  spreadsheetId: process.env.SPREADSHEET_ID,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectURL: process.env.REDIRECT_URL,
+  accessToken: process.env.ACCESS_TOKEN,
+  refreshToken: process.env.REFRESH_TOKEN
 }
