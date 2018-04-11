b6f4e6800c114f80167bd2c7cc72447cac9d8fed
Emmanuel Singletary <me@manny.co>
2018-04-11 12:21:42 -0400

    Added travis info for CI testing

--- sql/section.sql
+++ sql/section.sql
@@ -1,9 +1,20 @@
+CREATE DATABASE sla_test_db;
+
+USE sla_test_db;
+
 CREATE TABLE terms (
   term    VARCHAR(3),
   start_date  DATE,
   end_date  DATE,
   PRIMARY KEY (term)
 );
+
+INSERT INTO terms (term, start_date, end_date) VALUES
+  ('A6', '2018-05-07', '2018-06-17'),
+  ('A8', '2018-05-07', '2018-07-01'),
+  ('B6', '2018-06-18', '2018-08-05'),
+  ('B8', '2018-06-18', '2018-08-19'),
+  ('C6', '2018-07-09', '2018-08-19');
 
 CREATE TABLE sections (
   course_id   VARCHAR(10),
@@ -18,10 +29,3 @@ CREATE TABLE sections (
   PRIMARY KEY (course_id, section),
   FOREIGN KEY (term) REFERENCES terms(term)
 );
-
-INSERT INTO terms (term, start_date, end_date) VALUES
-  ('A6', '2018-05-07', '2018-06-17'),
-  ('A8', '2018-05-07', '2018-07-01'),
-  ('B6', '2018-06-18', '2018-08-05'),
-  ('B8', '2018-06-18', '2018-08-19'),
-  ('C6', '2018-07-09', '2018-08-19');
