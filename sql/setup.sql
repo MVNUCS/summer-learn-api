7a540b609c496410963bbc632023f12f01f79154
Emmanuel Singletary <me@manny.co>
2018-04-24 23:23:46 -0400

    whoops sql error

--- sql/setup.sql
+++ sql/setup.sql
@@ -43,7 +43,7 @@ INSERT INTO fulfillment (intent, fulfillmentText, fulfillmentError, fulfillmentF
   ('coursesOfferedDuringTerm', 'The courses are as follows: #@#', 'It seems that there are not any courses offered during that term.', 1),
   ('canSummerLearnCoursesBeTransferred', 'Yes. MVNU makes the process very simple to request a transcript once your grades are posted for the Summer Learn courses.', NULL, 0),
   ('whatIsTheCostPerCreditHour', 'The cost per credit hour is $200', NULL, 0),
-  ('whenIsTheRegistrationDeadline', 'The registration deadline for that term is  #@#', 'It seems that there is not a registration deadline for that term!', 1);
-  ('learnAboutSummerLearn', 'I can help answer any questions you might have about summer learn. A few examples might be: "How do I register for summer learn", or "What classes are offered this summer?", or "What is the cost per credit hour?"', NULL, 0);
-  ('whatCoursesAreOffered', 'Summer Learn offers a wide variety of courses, including popular ones like Narrative of Scripture, Western Tradition, Science and the Modern Mind, and Christian Beliefs. Would you like to hear the complete list?', NULL, 0);
+  ('whenIsTheRegistrationDeadline', 'The registration deadline for that term is  #@#', 'It seems that there is not a registration deadline for that term!', 1),
+  ('learnAboutSummerLearn', 'I can help answer any questions you might have about summer learn. A few examples might be: "How do I register for summer learn", or "What classes are offered this summer?", or "What is the cost per credit hour?"', NULL, 0),
+  ('whatCoursesAreOffered', 'Summer Learn offers a wide variety of courses, including popular ones like Narrative of Scripture, Western Tradition, Science and the Modern Mind, and Christian Beliefs. Would you like to hear the complete list?', NULL, 0),
   ('completeListOfCoursesOffered', 'There are a lot of courses! Here they are along with the terms they are offered in. #@#. Whew!', 'Uh oh, it looks like we encountered an issue on our end. Please try again later.', 1);
