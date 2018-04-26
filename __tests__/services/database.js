f532e713870b7ce448811250a1406e32f4a3e4cd
Emmanuel Singletary <me@manny.co>
2018-04-26 11:06:30 -0400

    Adding and clearing for database tests

--- __tests__/services/database.js
+++ __tests__/services/database.js
@@ -1,12 +1,10 @@
-/* global describe beforeAll test expect  */
+/* global describe beforeAll afterAll test expect  */
 'use strict'
 require('dotenv').config()
 
 const database = require('../../services/database')
 const mysql = require('mysql')
 const keys = require('../../config/keys')
-
-const testConnection = mysql.createConnection(keys.database)
 
 const insertTestCourses = `
   REPLACE INTO sections (course_id, section, title, term, instructor, inst_type, registered, cap, credits)
@@ -15,24 +13,46 @@ const insertTestCourses = `
          ('TST-1024', 1, 'Test Course Two', 'B6', 'T. Ester', 'F', 15, 45, 4),
          ('TST-4032', 1, 'Test Course Three', 'C6', 'T. Ester', 'F', 10, 75, 2)
 `
+const deleteTestCourses = `
+  DELETE FROM sections
+  WHERE course_id LIKE 'TST%'
+`
 
-function initMockCourses() {
+function initMockCourses () {
+  let testConnection = mysql.createConnection(keys.database)
   return new Promise((resolve, reject) => {
-    testConnection.connect((err, connection) => {
+    testConnection.connect((err) => {
       if (err) return reject(err)
-      connection.query(insertTestCourses, (err, results, fields) => {
+      testConnection.query(insertTestCourses, (err, results, fields) => {
         if (err) return reject(err)
         return resolve(results)
       })
-      connection.end()
+      testConnection.end()
+    })
+  })
+}
+
+function clearMockCourses () {
+  let testConnection = mysql.createConnection(keys.database)
+  return new Promise((resolve, reject) => {
+    testConnection.connect(err => {
+      if (err) return reject(err)
+      testConnection.query(deleteTestCourses, (err, results, fields) => {
+        if (err) return reject(err)
+        return resolve(results)
+      })
+      testConnection.end()
     })
   })
 }
 
 describe('Database', () => {
-
   beforeAll(() => {
     return initMockCourses()
+  })
+
+  afterAll(() => {
+    return clearMockCourses()
   })
 
   test('should be able to connect to the database', async () => {
@@ -43,43 +63,44 @@ describe('Database', () => {
   describe('getCourse', () => {
     test('should return a course when given a valid and existing course ID and section', async () => {
       let result = await database.getCourse('TST-1003', '2')
-      expect()
+      // console.log(result)
+      expect(result[0].course_id).toBe('TST-1003')
     })
 
-    test('should return an empty set when given a valid but non-existing course ID and section', async () => {
-      let result = await database.getCourse('TST-1234', '1')
-    })
+    // test('should return an empty set when given a valid but non-existing course ID and section', async () => {
+    //   let result = await database.getCourse('TST-1234', '1')
+    // })
 
-    test('should return an empty set when no courses exist in the database', async () => {
-      let result = await database.getCourse('TST-1234', '1')
-    })
+    // test('should return an empty set when no courses exist in the database', async () => {
+    //   let result = await database.getCourse('TST-1234', '1')
+    // })
 
-    test('should throw an error when given an invalid course ID and section', async () => {
-      let result = await database.getCourse('INVALID', 'INVALID')
-    })
+    // test('should throw an error when given an invalid course ID and section', async () => {
+    //   let result = await database.getCourse('INVALID', 'INVALID')
+    // })
   })
 
-  describe('getAllSectionsOfCourse', () => {
-    test('should return all sections of a course when given just a valid course ID', async () => {
-      let result = await database.getAllSectionsOfCourse('TST-1003')
-      expect()
-    })
+  // describe('getAllSectionsOfCourse', () => {
+  //   test('should return all sections of a course when given just a valid course ID', async () => {
+  //     let result = await database.getAllSectionsOfCourse('TST-1003')
+  //     expect()
+  //   })
 
-    test('should return an empty set when given a valid but non-existing course ID', async () => {
-      let result = await database.getAllSectionsOfCourse('TST-1234')
-    })
+  //   test('should return an empty set when given a valid but non-existing course ID', async () => {
+  //     let result = await database.getAllSectionsOfCourse('TST-1234')
+  //   })
 
-    test('should return an empty set when no courses exist in the database', async () => {
-      let result = await database.getCourse('TST-1234', '1')
-    })
+  //   test('should return an empty set when no courses exist in the database', async () => {
+  //     let result = await database.getCourse('TST-1234', '1')
+  //   })
 
-    test('should throw an error when given an invalid course ID', async () => {
-      let result = await database.getCourse('1234')
-    })
-  })
+  //   test('should throw an error when given an invalid course ID', async () => {
+  //     let result = await database.getCourse('1234')
+  //   })
+  // })
 
-  test('should return all courses', async () => {
-    let result = await database.getAllCourses()
-    expect()
-  })
+  // test('should return all courses', async () => {
+  //   let result = await database.getAllCourses()
+  //   expect()
+  // })
 })
